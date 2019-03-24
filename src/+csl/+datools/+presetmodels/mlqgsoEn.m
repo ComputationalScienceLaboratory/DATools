9d6845270935c8715bf1fa63887123e057bd79c4
Andrey A Popov <apopov@vt.edu>
2019-03-24 10:32:41 -0400

    I forgot
    Former-commit-id: d0055b009917e9923a8f4bd2a14708ffd071aa51

--- src/+csl/+datools/+presetmodels/mlqgsoEn.m
+++ src/+csl/+datools/+presetmodels/mlqgsoEn.m
@@ -1,9 +1,6 @@
 %% Create Nature
 
-natureode = otp.qgso.presets.Canonical( ...
-    'Size', 'large', ...
-    'Dissipation', 'low', ...
-    'XBoundaryCondition', 'C');
+natureode = otp.qg.presets.PopovSandu2019('Nature');
 %natureode = csl.odetestproblems.qgso.presets.GC('large', 'high');
 
 natureode.TimeSpan = [0, 5];
@@ -15,86 +12,29 @@ naturesyntherror = csl.datools.error.Error;
 nature = csl.datools.Model(natureode, solvernature, ...
     'SynthError', naturesyntherror);
 
-% create the nature-to-model operator
-nf = sqrt(natureode.NumVars);
-nf = 255;
-nc = (nf - 1)/2;
-rc = 1:nc;
-if2cs = [rc, rc, rc];
-jf2cs = [(2*rc)-1, 2*rc, (2*rc)+1];
-vf2cs = [1/4*ones(1, nc), 1/2*ones(1, nc), 1/4*ones(1, nc)];
-If2c1D = sparse(if2cs, jf2cs, vf2cs, nc, nf);
-If2c = kron(speye(nc), If2c1D) * kron(If2c1D, speye(nf));
-
-% create 127 to 63 operator
-nf = (nf - 1)/2;
-nc = (nf - 1)/2;
-rc = 1:nc;
-if2cs = [rc, rc, rc];
-jf2cs = [(2*rc)-1, 2*rc, (2*rc)+1];
-vf2cs = [1/4*ones(1, nc), 1/2*ones(1, nc), 1/4*ones(1, nc)];
-If2c1D = sparse(if2cs, jf2cs, vf2cs, nc, nf);
-If127_2_c63 = kron(speye(nc), If2c1D) * kron(If2c1D, speye(nf));
-Ic63_2_f127 = 4*If127_2_c63.';
-
-nf = (nf - 1)/2;
-nc = (nf - 1)/2;
-rc = 1:nc;
-if2cs = [rc, rc, rc];
-jf2cs = [(2*rc)-1, 2*rc, (2*rc)+1];
-vf2cs = [1/4*ones(1, nc), 1/2*ones(1, nc), 1/4*ones(1, nc)];
-If2c1D = sparse(if2cs, jf2cs, vf2cs, nc, nf);
-If63_2_c31 = kron(speye(nc), If2c1D) * kron(If2c1D, speye(nf));
-Ic31_2_f63 = 4*If63_2_c31.';
-
-nf = (nf - 1)/2;
-nc = (nf - 1)/2;
-rc = 1:nc;
-if2cs = [rc, rc, rc];
-jf2cs = [(2*rc)-1, 2*rc, (2*rc)+1];
-vf2cs = [1/4*ones(1, nc), 1/2*ones(1, nc), 1/4*ones(1, nc)];
-If2c1D = sparse(if2cs, jf2cs, vf2cs, nc, nf);
-If31_2_c15 = kron(speye(nc), If2c1D) * kron(If2c1D, speye(nf));
-Ic15_2_f31 = 4*If31_2_c15.';
-
-
-
-%naturetomodel = csl.datools.observation.Linear(nature.NumVars, 'H', If2c);
-
 naturetomodel = csl.datools.observation.Observation(nature.NumVars);
 
 %% Create model hierarchy
 
 % The lowest level will be a DMD based model.
-load('qgdmd.mat');
-load('qgresnn_best.mat');
+%load('qgresnn_best.mat');
 load('qg_pod_basis.mat');
 
-modelode{1}  = otp.qgso.presets.Canonical( ...
-    'Size', 'large', ...
-    'Dissipation', 'high', ...
-    'XBoundaryCondition', 'C');
-%modelode{1}.Parameters.linearsolver = 'multigrid';
-%modelode{1}.Parameters.linearsolvertol = 1e-2;
+modelode{1} = otp.qg.presets.PopovSandu2019('POD');
+
+
 
 modelode{1}.TimeSpan = [0 5];
-%hm = 0.25;
-%solvermodel{1} = @(f, t, y) csl.utils.rk4(f, t, y, round(diff(t)/hm));
-%solvermodel{1} = @(f, t, y) ode45(f, t, y);
-%solvermodel{1} = @(~, t, y) ode45(@(t, yy) dmdprop{4}(yy), t, y);
-%solvermodel{1} = @(~, t, y) deal(t, [y, Ic63_2_f127*dmdprop{5}(If127_2_c63*y)].');
-%solvermodel{1} = @(~, t, y) deal(t, [y, Ic63_2_f127*(Ic31_2_f63*(Ic15_2_f31*qgresmodel.run(If31_2_c15*(If63_2_c31*(If127_2_c63*y)))))].');
-
-
-% Good
-%solvermodel{1} = @(~, t, y) deal(t, [y, Ic63_2_f127*(Ic31_2_f63*(qgresmodel.run((If63_2_c31*(If127_2_c63*y)))))].');
 
 % POD
 modelode{1}.Parameters.pod = struct('basis', U, 'arakawabasis', UArakawa, 'arakawaDEIM', PArakawa);
 
 %solvermodel{1} = @(~, t, y) modelode{1}.fromPOD(ode45(modelode{1}.RhsPOD.F, t, U'*y));
-hm = 1;
-solvermodel{1} = @(~, t, y) modelode{1}.fromPOD(csl.utils.rk4(modelode{1}.RhsPOD.F, t, U'*y, round(diff(t)/hm)));
+hm = 1.25;
+solvermodel{1} = @(~, t, y) modelode{1}.fromPOD(csl.utils.rk4_filter(modelode{1}.RhsPOD.F, t, U'*y, round(diff(t)/hm), modelode{1}.Filter));
+
+%solvermodel{1} = @(~, t, y) modelode{1}.fromPOD(csl.utils.rk4(modelode{1}.RhsPOD.F, t, U'*y, round(diff(t)/hm)));
+
 
 %hm = 0.25;
 %solvermodel{1} = @(f, t, y) csl.utils.rk4(@(t, yy) dmdprop{1}(yy), t, y, round(diff(t)/hm));
@@ -103,10 +43,8 @@ model{1}  = csl.datools.Model(modelode{1},  solvermodel{1});
 
 % The highest level will be a standard QG model
 
-modelode{2}  = otp.qgso.presets.Canonical( ...
-    'Size', 'large', ...
-    'Dissipation', 'high', ...
-    'XBoundaryCondition', 'C');
+modelode{2} = otp.qg.presets.PopovSandu2019('Model');
+
 %modelode{2}.Parameters.e = 1e-7;
 %modelode{2}.Parameters.linearsolver = 'multigrid';
 %modelode{2}.Parameters.linearsolvertol = 1e-7;
@@ -121,12 +59,9 @@ solvermodel{2} = @(f, t, y) csl.utils.rk4(f, t, y, round(diff(t)/hm));
 model{2}  = csl.datools.Model(modelode{2},  solvermodel{2});
 
 % Control model for DEnKF
-modelodeC  = otp.qgso.presets.Canonical( ...
-    'Size', 'large', ...
-    'Dissipation', 'high');
+modelodeC = otp.qg.presets.PopovSandu2019('Model');
 modelodeC.Parameters = modelode{2}.Parameters;
 %modelodeC.Parameters = modelode{1}.Parameters;
-
 
 modelodeC.TimeSpan = [0 5];
 solvermodelC = solvermodel{2};
@@ -135,12 +70,16 @@ modelC  = csl.datools.Model(modelodeC,  solvermodelC);
 
 %% Create observations
 
+% no = 300;
 no = 300;
+
+% we only want to observe the top layer
+nvar = model{2}.NumVars/model{2}.ODEModel.Parameters.nlayers;
 
 observeindicies = round(linspace(1, model{2}.NumVars, no));
 nobsvars = numel(observeindicies);
 
-R = 4*speye(nobsvars);
+R = 1*speye(nobsvars);
 
 obserrormodel = csl.datools.error.Gaussian('CovarianceSqrt', sqrtm(R));
 observation = csl.datools.observation.Indexed(model{2}.NumVars, ...
@@ -150,11 +89,11 @@ observation = csl.datools.observation.Indexed(model{2}.NumVars, ...
 
 %% Do the rest
 
-ensN = 25;
+ensN = 12;
 distfn = model{end}.DistanceFunction;
 
 % We make the assumption that there is no model error
 modelerror = csl.datools.error.Error;
 
-load('qgsolargeensemble.mat');
+load('qglargeensemble.mat');
 ensembleGenerator = @(x) ysamples(:, randperm(size(ysamples, 2), x));
