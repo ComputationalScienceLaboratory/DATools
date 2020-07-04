17deb42338961cd36cb7910f4f5d21996442998e
Andrey A Popov <apopov@vt.edu>
2020-07-03 23:19:09 -0400

    Added ETPF and FETPF

--- src/+datools/+statistical/+ensemble/ETPF.m
+++ src/+datools/+statistical/+ensemble/ETPF.m
@@ -1,55 +1,43 @@
 classdef ETPF < datools.statistical.ensemble.EnF
     
-    
     methods
         
         function analysis(obj, R, y)
-            
-            tau = obj.Inflation^2 - 1;
+
+            % abuse 
+            tau = obj.Inflation;
             
             tc = obj.Model.TimeSpan(1);
             
             xf = obj.Ensemble;
             ensN = obj.NumEnsemble;
             
-            xfm = mean(xf, 2);
-            Af = xf - repmat(xfm, 1, ensN);
-            
+            AeqT = [kron(speye(ensN), ones(1, ensN)); kron(ones(ensN, 1), speye(ensN)).'];
+            lbT = zeros((ensN)*ensN, 1);
+            optsT = optimoptions('linprog', 'Display', 'off');
+           
             Hxf = obj.Observation.observeWithoutError(tc, xf);
             
             xdist = zeros(ensN, ensN);
-            weights = zeros(ensN, 1);
+            w = zeros(ensN, 1);
             
             for i = 1:ensN
                 xtemp = xf - repmat(xf(:, i), 1, ensN);
-                xtemp = xtemp .* xtemp;
-                xdist(i, :) = sum(xtemp);
+                xdist(i, :) = vecnorm(xtemp).^2;
                 
                 inn = y - Hxf(:, i);
-                weights(i) = exp(-0.5*inn'*(R\inn));
+                w(i) = exp(-0.5*inn'*(R\inn));
             end
             
+            w = w/sum(w);
             
-            weights = weights/sum(weights)
+            beqT = [ones(ensN, 1)/ensN; w];
+            f = xdist(:);
+            Tx = linprog(f, [], [], AeqT, beqT, lbT, [], optsT);
+            Tx = ensN*reshape(Tx, ensN, ensN);
             
-            T = optimvar('T', ensN, ensN, 'LowerBound', 0);
-            prob = optimproblem('Objective', sum(sum(T.*xdist)), 'ObjectiveSense', 'min');
-            opts = optimoptions(prob);
-            opts.Display = 'off';
-%             opts.Algorithm = 'interior-point';
-            prob.Constraints.c1 = sum(T) == ones(1, ensN)/ensN;
-            prob.Constraints.c2 = sum(T, 2) == weights;
-            prob.Constraints.c3 = sum(sum(T)) == 1;
-            problem = prob2struct(prob, 'Options', opts);
-            [sol, ~, ~, ~] = linprog(problem);
-            
-            Tx = reshape(sol, ensN, ensN);
-            Tx = ensN*Tx;
-            
-            xa = xf*Tx;
-            
-            B = tau/(ensN - 1)*(Af*Af.');
-            xa = xa + sqrtm(B)*randn(size(xa));
+            P = sqrt(tau/(ensN - 1))*(eye(ensN) - ones(ensN)/ensN)*randn(ensN)*(eye(ensN) - ones(ensN)/ensN);
+            xa = xf*(Tx + P);
             
             obj.Ensemble = xa;
             obj.Model.update(0, obj.BestEstimate);
---
+++ src/+datools/+statistical/+ensemble/FETPF.m
@@ -0,0 +1,120 @@
+classdef FETPF < datools.statistical.ensemble.EnF
+    
+    properties
+        B
+        Bsqrt
+        SurrogateEnsN
+        Laplace
+    end
+    
+    methods
+        function obj = FETPF(varargin)
+            
+            p = inputParser;
+            p.KeepUnmatched = true;
+            addParameter(p, 'B', 1);
+            addParameter(p, 'SurrogateEnsembleSize', 2);
+            addParameter(p, 'Laplace', false);
+            
+            parse(p, varargin{2:end});
+            
+            s = p.Results;
+            
+            kept = p.Unmatched;
+            
+            obj@datools.statistical.ensemble.EnF(varargin{1}, kept);
+            
+            obj.B = s.B;
+            obj.Bsqrt = sqrtm(s.B);
+            obj.SurrogateEnsN = s.SurrogateEnsembleSize;
+            obj.Laplace = s.Laplace;
+      
+        end
+    end
+    
+    methods
+        
+        function analysis(obj, R, y)
+
+            % abuse 
+            inflation = obj.Inflation;
+            M = obj.SurrogateEnsN;
+            
+            tc = obj.Model.TimeSpan(1);
+            
+            xf = obj.Ensemble;
+            ensN = obj.NumEnsemble;
+            
+            AeqT = [kron(speye(ensN), ones(1, ensN + M)); kron(ones(ensN, 1), speye(ensN + M)).'];
+            lbT = zeros((ensN + M)*ensN, 1);
+            optsT = optimoptions('linprog', 'Display', 'off');
+           
+            
+            
+            xfm = mean(xf, 2);
+            Af = xf - xfm;
+            
+            Covsqrt = (1/sqrt(ensN - 1)*Af);           
+            Bsqrtf = obj.Bsqrt;
+     
+            s = svd(Bsqrtf\Covsqrt);
+            trC = sum(s.^2);
+            tr2C = trC*trC;
+            trC2 = sum(s.^4);
+            
+            n = size(xf, 1);
+            
+            
+            NN = ensN - 1;
+            gamma = min(1, ((NN - 2)/NN * trC2 + tr2C)/((NN + 2)*(trC2 - tr2C/n)));
+            mu = trC/n;
+            
+            %mu =  sum(s1.^2)/sum(s2.^2);
+            Asynth = sqrt(mu)*Bsqrtf*randn(n, M);
+            
+            laplace = obj.Laplace;
+            if laplace
+                Z = exprnd(1, 1, M);
+                Asynth = Z.*Asynth;
+            end
+            
+            Asynth = Asynth - mean(Asynth, 2);
+            
+            Afrak = [Af, inflation*Asynth];
+            chiF = xfm + Afrak;
+            
+            Hchif = obj.Observation.observeWithoutError(tc, chiF);
+            
+            xdist = zeros(size(chiF));
+            w = zeros(ensN + M, 1);
+            
+            for i = 1:(ensN + M)
+                X_temp = chiF - chiF(:, i);
+                xdist(i, :) = vecnorm(X_temp, 2).^2;
+                inn = y - Hchif(:, i);
+                w(i) = exp(-0.5*inn'*(R\inn));
+                
+            end
+            
+            xdist = xdist(:, 1:ensN);
+            
+            w(1:ensN) = w(1:ensN)*(1 - gamma)/(ensN - 1);
+            w((ensN + 1):end) = w((ensN + 1):end)*gamma/(M - 1);
+            
+            w = w/sum(w);
+            
+            beqT = [ones(ensN, 1)/ensN; w];
+            f = xdist(:);
+            Tx = linprog(f, [], [], AeqT, beqT, lbT, [], optsT);
+            Tx = ensN*reshape(Tx, ensN + M, ensN);
+            
+            xa = chiF*(Tx);
+            
+            obj.Ensemble = xa;
+            obj.Model.update(0, obj.BestEstimate);
+            
+        end
+        
+    end
+    
+end
