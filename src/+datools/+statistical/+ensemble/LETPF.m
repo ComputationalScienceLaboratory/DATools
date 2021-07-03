5d1cbed58e94100f7f09cca7eef2b7972236b4be
Andrey A Popov <apopov@vt.edu>
2021-07-03 14:50:22 -0400

    Added LETPF

---
+++ src/+datools/+statistical/+ensemble/LETPF.m
@@ -0,0 +1,69 @@
+classdef LETPF < datools.statistical.ensemble.EnF
+    
+    methods
+        
+        function analysis(obj, R, y)
+
+            tau = obj.Rejuvenation;
+            
+            tc = obj.Model.TimeSpan(1);
+            
+            xf = obj.Ensemble;
+            n = size(xf, 1);
+            ensN = obj.NumEnsemble;
+            
+            AeqT = [kron(speye(ensN), ones(1, ensN)); kron(ones(ensN, 1), speye(ensN)).'];
+            lbT = zeros((ensN)*ensN, 1);
+            optsT = optimoptions('linprog', 'Display', 'off');
+           
+            Hxf = obj.Observation.observeWithoutError(tc, xf);
+            
+            xdist = zeros(ensN, ensN);
+            
+            for i = 1:ensN
+                xtemp = xf - repmat(xf(:, i), 1, ensN);
+                xdist(i, :) = vecnorm(xtemp).^2;
+            end
+            
+            t0 = Hxf - y;
+            
+            xa = xf;
+            
+            invR = spdiags(1./diag(R), 0, size(R, 1), size(R, 2));
+            
+            for k = 1:n
+               
+
+                if isempty(obj.Localization)
+                    C = speye(size(invR));
+                else
+                    C = obj.Localization(tc, xfm, Hi, k);
+                end
+                
+                w = exp(-0.5*sum(t0.*((C*invR)*t0), 1)).';
+                w = w/sum(w);
+                
+                beqT = [ones(ensN, 1)/ensN; w];
+                f = xdist(:);
+                Tx = linprog(f, [], [], AeqT, beqT, lbT, [], optsT);
+                Tx = ensN*reshape(Tx, ensN, ensN);
+                
+               
+                xa(k, :) = xf(k, :)*Tx;
+                
+            end
+            
+            P = sqrt(tau/(ensN - 1))*(eye(ensN) - ones(ensN)/ensN)*randn(ensN)*(eye(ensN) - ones(ensN)/ensN);
+            
+            xa = xa + xf*P;
+            
+            obj.Ensemble = xa;
+            obj.Model.update(0, obj.BestEstimate);
+            
+            obj.Weights = ones(ensN, 1)/ensN;
+            
+        end
+        
+    end
+    
+end
