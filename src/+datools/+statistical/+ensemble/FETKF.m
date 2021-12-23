394d58d5f0b4cd5ed65fc0f7b695250dc407c4bd
Andrey A Popov <apopov@vt.edu>
2021-12-23 09:22:31 -0500

    FETKF commit

---
+++ src/+datools/+statistical/+ensemble/FETKF.m
@@ -0,0 +1,158 @@
+classdef FETKF < datools.statistical.ensemble.EnF
+
+    properties
+        B
+        Bsqrt
+        BsqrtD
+        SurrogateEnsN
+        Laplace
+        Gamma
+    end
+
+    methods
+        function obj = FETKF(varargin)
+            p = inputParser;
+            p.KeepUnmatched = true;
+            addParameter(p, 'B', 1);
+            addParameter(p, 'Bsqrt', 1)
+            addParameter(p, 'SurrogateEnsembleSize', 2);
+            addParameter(p, 'Laplace', false);
+
+            parse(p, varargin{2:end});
+
+            s = p.Results;
+
+            kept = p.Unmatched;
+
+            obj@datools.statistical.ensemble.EnF(varargin{1}, kept);
+
+            obj.B      = s.B;
+            obj.Bsqrt  = s.Bsqrt;
+            obj.BsqrtD = decomposition(obj.Bsqrt, 'chol');
+
+            obj.SurrogateEnsN = s.SurrogateEnsembleSize;
+            obj.Laplace = s.Laplace;
+
+        end
+
+        function forecast(obj)
+
+            times = zeros(obj.NumEnsemble, 1);
+
+            if obj.Parallel
+                rhs = obj.Model.ODEModel.Rhs.F;
+                tspan = obj.Model.TimeSpan;
+                solver = obj.Model.Solver;
+                ens    = obj.Ensemble;
+                ensN   = obj.NumEnsemble;
+
+                parfor ensi = 1:ensN
+
+                    [t, y] = solver(rhs, tspan, ens(:, ensi));
+
+                    time = t(end) - t(1);
+                    yend = y(end, :).';
+
+                    ens(:, ensi) = yend;
+                    times(ensi) = time;
+                end
+
+                for ensi = 1:ensN
+                    obj.Ensemble(:, ensi) = obj.ModelError.adderr(obj.Model.TimeSpan(end), ens(:, ensi));
+                end
+
+            else
+                for ensi = 1:obj.NumEnsemble
+                    [time, yend] = obj.Model.solve([], obj.Ensemble(:, ensi));
+
+                    obj.Ensemble(:, ensi) = obj.ModelError.adderr(obj.Model.TimeSpan(end), yend);
+                    times(ensi) = time;
+                end
+            end
+
+            obj.Model.update(mean(times), obj.BestEstimate);
+
+        end
+
+        function analysis(obj, R, y)
+
+
+            inflation = obj.Inflation;
+
+            tc = obj.Model.TimeSpan(1);
+
+            xf = obj.Ensemble;
+            ensN = obj.NumEnsemble;
+
+
+            xfm = mean(xf, 2);
+            n = numel(xfm);
+
+
+            Af = xf - repmat(xfm, 1, ensN);
+
+            Af = inflation*Af;
+
+            Bs = obj.Bsqrt;
+            BsD = obj.BsqrtD;
+
+            s = svd(BsD\Af/sqrt(ensN - 1));
+            trBPf = sum(s.^2);
+            trBPf2 = sum(s.^4);
+
+            ensNN = ensN - 1;
+
+            if isempty(obj.Gamma)
+                % RBLW
+                gamma = min((((ensNN - 2)/ensNN) * trBPf2 + trBPf^2)/((ensNN + 2)*(trBPf2 - (1/n)*trBPf^2)), 1);
+                % OAS
+                %gamma = max(min(( (1 - 2/n)*trBPf2 + trBPf^2 )/( (ensNN + 1 - 2/n)*(trBPf2 - (trBPf^2)/n) ), 1), 0);
+            else
+                gamma = obj.Gamma;
+            end
+            mu = trBPf/n;
+
+            ensNW = obj.SurrogateEnsN;
+
+            xf = repmat(xfm, 1, ensN) + Af;
+
+            Hxf = obj.Observation.observeWithoutError(tc, xf);
+            Hxfm = mean(Hxf, 2);
+
+            HAf = Hxf - repmat(Hxfm, 1, ensN);
+
+            Afs = sqrt(1 - gamma)*Af/sqrt(ensN - 1);
+
+            w = sqrt(mu)*Bs*randn(n, ensNW);
+            w = w - repmat(mean(w, 2), 1, ensNW);
+            ws = sqrt(gamma)*w/sqrt(ensNW - 1);
+
+            Hw = obj.Observation.observeWithoutError(tc, w + repmat(xfm, 1, ensNW));
+            Hwm = mean(Hw, 2);
+            HAw = Hw - repmat(Hwm, 1, ensNW);
+
+            ZA = sqrt(1-gamma)*HAf/sqrt(ensN - 1);
+            Zw = sqrt(gamma)*HAw/sqrt(ensNW - 1);
+
+            S = ZA*ZA' + Zw*Zw' + R;
+
+            ZZ = [ZA, Zw];
+
+            TT = (eye(ensN + ensNW) - ZZ.'/S*ZZ);
+
+            d = y - Hxfm;
+
+            AAa = [Afs, ws]*real(sqrtm(TT));
+            Aa = sqrt(ensN - 1)*AAa(:, 1:ensN)/sqrt(1-gamma);
+
+            xam = xfm + ([Afs, ws]*TT*ZZ.')/R*d;
+
+            obj.Ensemble = repmat(xam, 1, ensN) + Aa;
+
+            obj.Model.update(0, obj.BestEstimate);
+
+        end
+
+    end
+
+end
